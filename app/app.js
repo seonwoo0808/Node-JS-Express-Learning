24dfeb59d56b86bd139f870b3532eae4cb9c3082
seonwoo0808 <seonwoo0808@naver.com>
2022-03-29 08:41:42 +0900

    add js for front-end

--- app/app.js
+++ app/app.js
@@ -7,6 +7,8 @@ const app = express();
 app.set("views", "./app/src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`))
+
 const home = require("./src/routes/home")
 app.use("/", home);
 
