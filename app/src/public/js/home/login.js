9fd7e075e1ce0e0cd2a384e43b955629b34cfd7b
seonwoo0808 <seonwoo0808@naver.com>
2022-03-29 17:33:33 +0900

    edit login fetch

---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("#login-button");
+
+loginBtn.addEventListener("click")
+
+function login() {
+    const req = {
+        id : id.value,
+        pw : pw.value
+    }
+    fetch("/login", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(req)
+    }
+    )
+}
