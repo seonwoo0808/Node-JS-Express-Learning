2472121920ab21d7399780fb0ef208be5b72bf7a
seonwoo0808 <seonwoo0808@naver.com>
2022-03-31 09:02:19 +0900

    add body parser

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 app.set("views", "./app/src/views");
 app.set("view engine", "ejs");
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl")
 
-router.get("/",ctrl.index);
+router.get("/",ctrl.output.index);
 
-router.get("/login",(ctrl.login));
+router.get("/login",(ctrl.output.login));
 
+router.post("/login",(ctrl.process.login));
 module.exports = router;
